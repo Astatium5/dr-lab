80cfbf249ce246071d1090759f28d2fcfe2bbaef
Armintron <64503268+Armintron@users.noreply.github.com>
2021-03-28 00:29:21 -0400

    fix multer

--- server/src/routes/visit.js
+++ server/src/routes/visit.js
@@ -10,10 +10,11 @@ import multer from 'multer';
 import controller from '../controllers/visit';
 
 const visit = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 visit.post('/', controller.create);
 visit.delete('/', controller.delete);
 visit.put('/', controller.update);
-visit.put('/upload', multer.array('photos'), controller.uploadPhotos);
+visit.put('/upload', upload.array('photos'), controller.uploadPhotos);
 
 export default visit;
