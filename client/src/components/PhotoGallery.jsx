301bae7d63279d6b14df17e03b362d857c9a9e78
ctcuff <dev.ctcuff@gmail.com>
2021-03-28 07:08:31 -0400

    Added image uploading

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -14,29 +14,10 @@ import placeholderImage from '../assets/dummy-image.jpg';
 function PhotoGallery({ visitID }) {
   const [imageList, setImages] = useState([]);
 
-  const uploadImage = (value) => {
-    const formData = new FormData();
-    console.log(value.file);
-    formData.set('photo', value.file);
-    formData.set('body', '');
-    console.log(value.file.name);
-    window.fetch(`/visits/photos/${visitID}`, {
-      method: 'PUT',
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((url) => {
-        imageList.push(url);
-        setImages(imageList);
-      });
-  };
-
   useEffect(() => {
-    console.log('got here');
     window.fetch(`/visits/photos/${visitID}`)
       .then((response) => response.json())
       .then((url) => {
-        console.log(url);
         setImages(url);
       });
   }, []);
@@ -47,8 +28,20 @@ function PhotoGallery({ visitID }) {
         <h1>Patient Photos</h1>
         <Tooltip placement="top" title="Add photo">
           <Upload
-            action={`/visits/photos/${visitID}`}
-            onChange={uploadImage}
+            customRequest={(options) => {
+              const formData = new FormData();
+              formData.append('photo', options.file);
+
+              window.fetch(`/visits/photos/${visitID}`, {
+                method: 'PUT',
+                body: formData,
+              })
+                .then((response) => response.json())
+                .then((url) => {
+                  imageList.push(url);
+                  setImages(imageList);
+                });
+            }}
             fileList={null}
             accept="image/png, image/jpeg, image/jpg"
           >
