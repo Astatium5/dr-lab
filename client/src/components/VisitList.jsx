aa8b42506a415c34b982b40e2addfa0112fffe61
ctcuff <dev.ctcuff@gmail.com>
2021-03-28 00:20:18 -0400

    Added ability to upload visits

--- client/src/components/VisitList.jsx
+++ client/src/components/VisitList.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import '../scss/visit-list.scss';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { List } from 'antd';
+import {
+  List,
+  Button,
+  Modal,
+  Calendar,
+} from 'antd';
 
 function VisitList({ visits }) {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const closeModal = () => setModalVisible(false);
+  const openModal = () => setModalVisible(true);
+
   return (
-    <List
-      className="visit-list"
-      dataSource={visits}
-      header={<div>Visits</div>}
-      renderItem={(item) => (
-        <List.Item>
-          {item}
-        </List.Item>
-      )}
-    />
+    <div className="visit-list">
+      <List
+        dataSource={visits}
+        header={<div>Visits</div>}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+      <Button type="primary" className="btn-add" onClick={openModal}>
+        Add Visit
+      </Button>
+      <Modal
+        className="date-modal"
+        title="Choose Visit Date"
+        visible={isModalVisible}
+        onOk={closeModal}
+        onCancel={closeModal}
+      >
+        <Calendar fullscreen={false} onChange={(moment) => console.log(moment.format('l'))} />
+      </Modal>
+    </div>
   );
 }
 
