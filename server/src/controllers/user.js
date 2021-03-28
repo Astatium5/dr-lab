7b5454102856eb8693372f44f928c3345defba03
Armintron <64503268+Armintron@users.noreply.github.com>
2021-03-28 01:27:45 -0400

    Update user.js

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -6,13 +6,13 @@ import logger from '../util';
 const User = {
   register: async (req, res) => {
     const {
-      email, firstName, lastName, specialty, clinic,
+      email, firstName, lastName, clinic,
     } = req.body;
 
     const password = await hasher.hash(req.body.password);
 
     await db.collection('users').doc(email).set({
-      password, firstName, lastName, specialty, clinic,
+      password, firstName, lastName, clinic,
     });
 
     let addedUser = await db.collection('users').doc(email).get();
@@ -28,7 +28,7 @@ const User = {
     const user = await db.collection('users').doc(email).get();
 
     if (!user) return res.status(404).send({ message: 'User could not be found.' });
-    if (!hasher.validateHash(password, user.password)) {
+    if (!await hasher.validateHash(password, user.password)) {
       return res.status(404).send({ message: 'The specified password is not correct.' });
     }
 
