32e52833b3cada430ffb574372550eadf16296e5
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-03-28 10:45:14 -0400

    Update package.json

--- server/src/services/webServer.js
+++ server/src/services/webServer.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import router from '../routes/index';
@@ -16,13 +17,20 @@ const initWebServer = async () => {
     router,
   );
 
+  console.log(path.join(path.resolve('../'), 'client', 'build', 'index.html'));
+
+  app.use(express.static(path.join(path.resolve('../'), 'client', 'build')));
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.join(path.resolve('../'), 'client', 'build', 'index.html'));
+  // });
+
   app.use(
-    '/',
+    '/apidocs',
     swaggerUI.serve,
   );
 
   app.get(
-    '/',
+    '/apidocs',
     swaggerUI.setup(specs(), { explorer: true }),
   );
 
