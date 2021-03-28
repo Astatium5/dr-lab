7d088136f8a052b7c06d0f20043e6e39af7493ae
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-03-27 20:51:26 -0400

    fixed some issues

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,9 @@
 import initWebServer from './services/webServer';
+import logger from './util';
 
 (async function main() {
   const app = await initWebServer();
 
   app.listen(5000);
+  logger.info('Server running!');
 }());
